beb9fa25b5b30ed9ef0948e5544ae0310f750878
typetypee <79076104+typetypee@users.noreply.github.com>
2021-06-14 11:01:43 -0500

    Create blobs.js

---
+++ blobs.js
@@ -0,0 +1,28 @@
+{
+"angry-blob": "https://typetypee.github.io/blobs/angry-blob.png",
+"pencil-blob": "https://typetypee.github.io/blobs/pencil-blob.png",
+"flying-blob": "https://typetypee.github.io/blobs/flying-guy.png",
+"confused-blob": "https://typetypee.github.io/blobs/confused.blob.png",
+"dead-blob": "https://typetypee.github.io/blobs/dead-red.png",
+"waving-blob": "https://typetypee.github.io/blobs/waving-blob.png",
+"crying-blob": "https://typetypee.github.io/blobs/crying=blob.png",
+"red-writing-blob": "https://typetypee.github.io/blobs/red-writing-blob.png",
+"shadow-blob": "https://typetypee.github.io/blobs/shadow-pencil-blob/png",
+"red-blob-uchiha": "https://typetypee.github.io/blobs/plaaaaaaabe.png",
+"clothes-wearing-blob": "https://typetypee.github.io/blobs/clothes-wearing-blob.png",
+"orange-write": "https://typetypee.github.io/blobs/orange-write.png",
+"yellow-crying-blob": "https://typetypee.github.io/blobs/yellow-crying-blob.png",
+"yellow-shocked": "https://typetypee.github.io/blobs/yellow-shocked.png",
+"green-dead-blob": "https://typetypee.github.io/blobs/green-dead-blob.png",
+"blue-flower": "https://typetypee.github.io/blobs/blue-flower.png",
+"blue-wave": "https://typetypee.github.io/blobs/blue-wave.png",
+"blue-plane": "https://typetypee.github.io/blobs/flying-guy-blue.png", 
+"cyan-blob": "https://typetypee.github.io/blobs/cyan-blob.png",
+"cyan-cry": "https://typetypee.github.io/blobs/cyan-cry.png",
+"cyan-sleeping": "https://typetypee.github.io/blobs/cyan-sleeping.png",
+"cyan-angry": "https://typetypee.github.io/blobs/angry-cyan-blob.png",
+"pink-blob": "https://typetypee.github.io/blobs/pink-blob.png",
+"pink-plane": "https://typetypee.github.io/blobs/pink-plane.png",
+""
+
+}
